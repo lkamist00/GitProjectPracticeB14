0875c45ec72151d73fef7e9f24dc4c559163f39a
lkamist00 <lkamist00@gmail.com>
2022-12-18 17:33:16 -0500

    version 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("First Version is complete");
+        System.out.println("Version is complete");
     }
 
 
